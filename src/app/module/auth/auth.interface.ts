4eb370143607bd722b1c58d78951b1afc69c5cba
RAMJAN <niloyahmadramjan@gmail.com>
2026-09-13 10:58:06 +0800

    implement biome and setup the project

--- src/app/module/auth/auth.interface.ts
+++ src/app/module/auth/auth.interface.ts
@@ -1,20 +1,20 @@
-import { UserRole } from '../../../generated/prisma/enums'
+import { UserRole } from "../../../generated/prisma/enums";
 
 export interface ILoginUserPayload {
-    identifier: string
-    password: string
+	identifier: string;
+	password: string;
 }
 
 export interface IRegisterUserPayload {
-    name: string
-    phone: string
-    email?: string
-    password: string
+	name: string;
+	phone: string;
+	email?: string;
+	password: string;
 }
 
 export interface IRequestUser {
-    userId: string
-    email: string | null
-    name: string
-    role: UserRole
+	userId: string;
+	email: string | null;
+	name: string;
+	role: UserRole;
 }
